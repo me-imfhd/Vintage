2eb603ccbf05014d7835ba05e52f34191e7492be
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-04 13:45:46 +0530

    fix

--- packages/ui/components/ThemeProvider.tsx
+++ packages/ui/components/ThemeProvider.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import * as React from "react";
+import { ReactNode } from 'react';
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import {Provider} from "@radix-ui/react-tooltip";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>
-      <Provider>{children}</Provider>
-    </NextThemesProvider>
-  );
+interface ThemeProviderPropsWithChildren extends ThemeProviderProps {
+ children: ReactNode;
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderPropsWithChildren) {
+ return (
+   <NextThemesProvider {...props}>
+     <Provider>{children}</Provider>
+   </NextThemesProvider>
+ );
 }
